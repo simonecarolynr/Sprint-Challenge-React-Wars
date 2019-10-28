c5a5b9eec60fdb6e1bac4dc6a1d57744cb09f0b5
simonecarolynr <simonecarolynr@gmail.com>
2019-10-27 19:51:38 -0600

    successful fetch, not getting any errors back but also not working, few kinks to work out

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'; 
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [data, setData] = useState({
+    
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+    const result = await axios(
+      'https://swapi.co/api/people/',
+      );
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+      setData(result.data.results);
+    }
+   
+      fetchData();
+
+
+  });
+
+  //let container = document.getElementById('root')
+  //container.append(starWarsChar(result.data.results))
+
+ let Persons = [];
+
+ Persons.push(data);
+
+ Persons.map(person => {
+   return starWarsChar(person);
+   
+ })
+
+  function starWarsChar(Object) {
+    const charCard = document.createElement('div')
+    let birthYear = document.createElement('p')
+    let created = document.createElement('p')
+    let edited = document.createElement('p')
+    let eyeColor = document.createElement('p')
+    let films = document.createElement('p')
+    let gender = document.createElement('p')
+    let hairColor = document.createElement('p')
+    let height = document.createElement('p')
+    let homeWorld = document.createElement('p')
+    let mass = document.createElement('p')
+    let name = document.createElement('p')
+    let skinColor = document.createElement('p')
+    let species = document.createElement('p')
+    let starships = document.createElement('p')
+    let vehicles = document.createElement('p')
+
+    //add class lists for styling purposes here
+
+    //text content
+    birthYear.textContent = Object.birth_year;
+    created.textContent = Object.created;
+    edited.textContent = Object.edited;
+    eyeColor.textContent = Object.eye_color;
+    gender.textContent = Object.gender;
+    hairColor.textContent = Object.hair_color;
+    height.textContent = Object.height;
+    homeWorld.textContent = Object.homeworld;
+    mass.textContent = Object.mass;
+    name.textContent = Object.name;
+    skinColor.textContent = Object.skin_color;
+    species.textContent = Object.species;
+
+    
+    charCard.appendChild(birthYear);
+    charCard.appendChild(created);
+    charCard.appendChild(edited);
+    charCard.appendChild(eyeColor);
+    charCard.appendChild(films);
+    charCard.appendChild(gender);
+    charCard.appendChild(hairColor);
+    charCard.appendChild(height);
+    charCard.appendChild(homeWorld);
+    charCard.appendChild(mass);
+    charCard.appendChild(name);
+    charCard.appendChild(skinColor);
+    charCard.appendChild(species);
+    charCard.appendChild(starships);
+    charCard.appendChild(vehicles);
+
+    return charCard;
+  } 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+    
     </div>
   );
 }
